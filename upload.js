643769018e470b8895d4d3c73fe4d4499960a839
k3w <33373189+k3rc@users.noreply.github.com>
2025-06-10 17:59:45 +0200

    Update upload.js

--- upload.js
+++ upload.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://loop-backend-production-a1b3.up.railway.app/";
+const BASE_URL = "https://loop-backend-production-a1b3.up.railway.app";
 
 document.getElementById("uploadForm").addEventListener("submit", async (e) => {
     e.preventDefault();
