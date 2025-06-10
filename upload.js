f15e6339c70c6f4f1535335af0524b5a5cfad6df
k3w <33373189+k3rc@users.noreply.github.com>
2025-06-10 17:28:55 +0200

    Create upload.js

---
+++ upload.js
@@ -0,0 +1,16 @@
+const BASE_URL = "https://loop-backend.up.railway.app";
+
+document.getElementById("uploadForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const fileInput = document.getElementById("musicFile");
+    const formData = new FormData();
+    formData.append("file", fileInput.files[0]);
+
+    const res = await fetch(`${BASE_URL}/upload`, {
+        method: "POST",
+        body: formData,
+    });
+
+    const data = await res.json();
+    alert("Uploaded: " + data.filename);
+});
