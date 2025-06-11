bf7c3e8843d4fce7859ec0c4beac33ee626e09a6
k3w <33373189+k3rc@users.noreply.github.com>
2025-06-11 21:02:54 +0200

    Update gradient.js

--- assets/gradient.js
+++ assets/gradient.js
@@ -1,1 +1,5 @@
-
+// loop-frontend/gradient.js
+document.addEventListener("DOMContentLoaded", () => {
+  // В будущем сюда можно добавить динамическую загрузку альбомов и треков
+  console.log("Loop WebApp Loaded");
+});
